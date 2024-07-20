a18e6720155042a76a5dcb848bd804f75e9f2bac
cristianrobledo <cristianrobledo14@gmail.com>
2024-07-20 13:47:38 -0300

    mi nombre

--- js/index.js
+++ js/index.js
@@ -5,3 +5,12 @@ function laCajaDePandora(num){
           return num.toString(16); // Convertir a hexadecimal
         }
     }
+
+
+    function cristianRobledo() {
+        return {
+          nombre: "Cristian Robledo",
+          edad: 30, // Reemplaza con tu edad real
+          nacionalidad: "Argentina" // Reemplaza con tu nacionalidad real
+        };
+      }
