df9de761f0a6b6da7bf94acf37aaf38d767e67c6
Germanreynoso <143376483+Germanreynoso@users.noreply.github.com>
2024-07-20 14:03:25 -0300

    Revert "German"

--- js/index.js
+++ js/index.js
@@ -1,33 +1,16 @@
-function convertirNumero(num) {
-    if (num % 2 === 0) {
-        return num.toString(2); // Convertir a binario
-    } else {
-        return num.toString(16); // Convertir a hexadecimal
-    }
-}
-
-function miInformacion() {
-    return {
-        nombre: 'Juan Pérez',
-        edad: 30,
-        nacionalidad: 'Argentina'
-    };
-}
-
-// Nueva función
 function laCajaDePandora(num){
-    if (num % 2 === 0) {
-      return num.toString(2); // Convertir a binario
-    } else {
-      return num.toString(16); // Convertir a hexadecimal
+        if (num % 2 === 0) {
+          return num.toString(2); // Convertir a binario
+        } else {
+          return num.toString(16); // Convertir a hexadecimal
+        }
     }
-}
 
-// Nueva función
-function cristianRobledo() {
-    return {
-      nombre: "Cristian Robledo",
-      edad: 30, // Reemplaza con tu edad real
-      nacionalidad: "Argentina" // Reemplaza con tu nacionalidad real
-    };
-  }
+
+    function cristianRobledo() {
+        return {
+          nombre: "Cristian Robledo",
+          edad: 30, // Reemplaza con tu edad real
+          nacionalidad: "Argentina" // Reemplaza con tu nacionalidad real
+        };
+      }
